5f3f0034f055a0c42e548658df5dd6d23c7029b0
Your NameSRVDATTA1986 <tagorerabindranath1861@gmail.com>
2023-11-09 13:44:48 +0530

    Put and Patch API has been implemented

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express=require("express");
-const cors=require("cors");
+const express = require("express");
+const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 
 
-const port=process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 
 
@@ -26,52 +26,119 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      //  await client.connect();
-       
-      const userCollection=client.db("BookCategory").collection("Category");
-      const bookCollection=client.db("AllBook").collection("Book");
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //  await client.connect();
 
-      app.get("/Category", async (req, res) => {
-        const result = await userCollection.find().toArray();
-        console.log(result);
-        res.send(result);
+    const userCollection = client.db("BookCategory").collection("Category");
+    const bookCollection = client.db("AllBook").collection("Book");
 
-      });
+    app.get("/Category", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      console.log(result);
+      res.send(result);
 
-      app.post("/Book", async (req, res) => {
-        const user = req.body;
-           console.log(user);
-        const result = await bookCollection.insertOne(user);
-        console.log(result);
-        res.send(result);
-      });
+    });
 
-      app.get("/Book", async (req, res) => {
-        const result = await bookCollection.find().toArray();
-        console.log(result);
-        res.send(result);
+    app.post("/Book", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await bookCollection.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
 
-      });
+    app.get("/Book", async (req, res) => {
+      const result = await bookCollection.find().toArray();
+      console.log(result);
+      res.send(result);
 
-      app.get("/Books/:id", async(req,res)=>{
+    });
+
+    app.patch("/Book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      //  const options={upsert:true};
+      const updatedProduct = req.body;
+      if (!isNaN(updatedProduct.quantity)) {
+    updatedProduct.quantity = parseInt(updatedProduct.quantity);
+  } else {
+    updatedProduct.quantity = 0;
+  }
+      const product = {
+        $set: {
+          Quantity: updatedProduct.quantity,
+
+        }
+      }
+      const result = await bookCollection.updateOne(filter, product);
+      res.send(result);
+    })
+
+    app.get("/Book/:id", async(req,res)=>{
         const id=req.params.id;
         const query={_id: new ObjectId(id)}
         const result=await bookCollection.findOne(query);
         res.send(result);
       })
 
-      const userCollection2=client.db("BorrowedBook").collection("BookCart");
-      app.post("/BookCart", async (req, res) => {
-        const user = req.body;
-           console.log(user);
-        const result = await userCollection2.insertOne(user);
-        console.log(result);
+    app.put("/Book/:id",async(req,res)=>{
+        const id=req.params.id;
+        const filter={_id:new ObjectId(id)}
+        const options={upsert:true};
+        const updatedProduct=req.body;
+        const product={
+          $set:{
+            Name:updatedProduct.Name,
+            Image:updatedProduct.Image,
+            Category:updatedProduct.Category,
+            authorName:updatedProduct.authorName,
+            Rating:updatedProduct.Rating,
+            Description:updatedProduct.Description,
+            Quantity:updatedProduct.Quantity
+          }
+        }
+        const result=await bookCollection.updateOne(filter,product,options);
         res.send(result);
-      });
+      })
 
-      await client.db("admin").command({ ping: 1 });
+    app.get("/Books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookCollection.findOne(query);
+      res.send(result);
+    })
+
+    const userCollection2 = client.db("BorrowedBook").collection("BookCart");
+    app.post("/BookCart", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection2.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/BookCart", async (req, res) => {
+      const result = await userCollection2.find().toArray();
+      console.log(result);
+      res.send(result);
+
+    });
+
+    app.delete("/BookCart/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("id", typeof (id));
+      const objectId = new ObjectId(id);
+      console.log(objectId);
+      const query = {
+        _id: objectId,
+      };
+      console.log(query);
+      const result = await userCollection2.deleteOne(query);
+      res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -81,10 +148,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get("/",(req,res)=>{
-    res.send("CRUD is running....");
+app.get("/", (req, res) => {
+  res.send("CRUD is running....");
 })
 
-app.listen(port,()=>{
-    console.log(`App is running on PORT: ${port}`);
+app.listen(port, () => {
+  console.log(`App is running on PORT: ${port}`);
 })
